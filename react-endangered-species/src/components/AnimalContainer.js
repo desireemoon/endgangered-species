a8476824ed5e76c9fdf80912a2514f482c90c7a2
desireemoon <desiree.dewysocki@gmail.com>
2019-10-03 10:15:58 -0400

    fixed spacing and alignment

--- react-endangered-species/src/components/AnimalContainer.js
+++ react-endangered-species/src/components/AnimalContainer.js
@@ -5,35 +5,43 @@ const ArticleContainer = (props) => {
 
     return (
 
-        <div className="check-testing">
-            <div className="">
+        <div className="">
+            <div className="animal-info-container">
                 <h2>{props.animal && props.animal.name}</h2>
-                <div className="img-container" >
+                <div className="animal-img-container" >
                     <img className="animal-img"src={`http://www.bloowatch.org/${props.animal && props.animal.image.url}`} alt={props.animal && props.animal.name} />
                 </div>
-                <h3>Scientific Name: {props.animal && props.animal.scientific_name}</h3>
-                <p>Description: {props.animal && props.animal.description}</p>
-                <h3>Where does the {props.animal && props.animal.name} live?</h3>
-                <p>Location: {props.animal && props.animal.location}</p>
-                <p>Habitat: {props.animal && props.animal.habitat}</p>
-                <h3>What's the {props.animal && props.animal.name}'s endangered status?</h3>
-                <p>Status: {props.animal && props.animal.status}</p>
-                <p>There are only around {props.animal && props.animal.population} left in the wild.</p>
+                <div className="info-sections">
+                    <h3>Scientific Name: {props.animal && props.animal.scientific_name}</h3>
+                    <p>Description: {props.animal && props.animal.description}</p>
+                </div>
+                <div className="info-sections">
+                    <h3>Where does the {props.animal && props.animal.name} live?</h3>
+                    <p>Location: {props.animal && props.animal.location}</p>
+                    <p>Habitat: {props.animal && props.animal.habitat}</p>
+                </div>
+                <div className="info-sections"> 
+                    <h3>What's the {props.animal && props.animal.name}'s endangered status?</h3>
+                    <p>Status: {props.animal && props.animal.status}</p>
+                    <p>There are only around {props.animal && props.animal.population} left in the wild.</p>
+                    </div>
             </div>
-            <div className="status-container">
+            <div className="status-section">
                 <h2>What Does The Status Really Mean?</h2>
                 <p>There are three status types each animal will fall into: vulnerable, endangered, and critically endangered. Below are explanations of each status:</p>
-                <div className="status-boxes">
-                    <h3>{props.animal && props.animal.status_legend[0].status}</h3>
-                    <p>{props.animal && props.animal.status_legend[0].description}</p>
-                </div>
-                <div className="status-boxes">
-                    <h3>{props.animal && props.animal.status_legend[1].status}</h3>
-                    <p>{props.animal && props.animal.status_legend[1].description}</p>
-                </div>
-                <div className="status-boxes">
-                    <h3>{props.animal && props.animal.status_legend[2].status}</h3>
-                    <p>{props.animal && props.animal.status_legend[2].description}</p>
+                <div className="status-container">
+                    <div className="status-boxes">
+                        <h3>{props.animal && props.animal.status_legend[0].status}</h3>
+                        <p>{props.animal && props.animal.status_legend[0].description}</p>
+                    </div>
+                    <div className="status-boxes">
+                        <h3>{props.animal && props.animal.status_legend[1].status}</h3>
+                        <p>{props.animal && props.animal.status_legend[1].description}</p>
+                    </div>
+                    <div className="status-boxes">
+                        <h3>{props.animal && props.animal.status_legend[2].status}</h3>
+                        <p>{props.animal && props.animal.status_legend[2].description}</p>
+                    </div>
                 </div>
             </div>
         </div>
--- react-endangered-species/src/components/Status.js
+++ react-endangered-species/src/components/Status.js
@@ -5,22 +5,23 @@ const Status = (props) => {
     // console.log("lets grab that legend", props.animals[0].status_legend);
     
     return (
-      <div className="status-container">
+      <div className="status-section">
           <h2>What Does The Status Really Mean?</h2>
-          <p
-          > Each animal list will either have the status of vulnerable, endangered, and critically endangered. Below are explanations of each status:</p>
-         <div className="status-boxes">
-            <h3>{props.animal && props.animal.status_legend[0].status}</h3>
-            <p>{props.animal && props.animal.status_legend[0].description}</p>
-        </div> 
-        <div className="status-boxes">
-            <h3>{props.animal && props.animal.status_legend[1].status}</h3>
-            <p>{props.animal && props.animal.status_legend[1].description}</p>
+          <p> Each animal list will either have the status of vulnerable, endangered, and critically endangered. Below are explanations of each status:</p>
+        <div className="status-container">
+            <div className="status-boxes">
+                <h3>{props.animal && props.animal.status_legend[0].status}</h3>
+                <p>{props.animal && props.animal.status_legend[0].description}</p>
             </div> 
-        <div className="status-boxes">
-            <h3>{props.animal && props.animal.status_legend[2].status}</h3>
-            <p>{props.animal && props.animal.status_legend[2].description}</p>
-        </div>
+            <div className="status-boxes">
+                <h3>{props.animal && props.animal.status_legend[1].status}</h3>
+                <p>{props.animal && props.animal.status_legend[1].description}</p>
+                </div> 
+            <div className="status-boxes">
+                <h3>{props.animal && props.animal.status_legend[2].status}</h3>
+                <p>{props.animal && props.animal.status_legend[2].description}</p>
+            </div>
+        </div> 
       </div>
     );
   }
